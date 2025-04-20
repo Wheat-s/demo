f607e29d2884fa76fb90dd4d4d57088026651e20
Wheat-s <wheat8989@gmail.com>
2025-04-20 11:58:05 +0800

    Initial Commit

---
+++ demo-xcode/demo_xcodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  demo_xcodeApp.swift
+//  demo-xcode
+//
+//  Created by Wheat on 4/20/25.
+//
+
+import SwiftUI
+
+@main
+struct demo_xcodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
