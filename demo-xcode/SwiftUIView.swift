a1290b5db51248753064d7479f2b93364d1bd2fa
Wheat-s <wheat8989@gmail.com>
2025-04-20 14:23:41 +0800

    123

---
+++ demo-xcode/SwiftUIView.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  demo-xcode
+//
+//  Created by Wheat on 4/20/25.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
